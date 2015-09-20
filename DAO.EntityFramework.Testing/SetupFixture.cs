55ebbde41f1546386d95b4303f524a3721cffa90
lvalido <lvalido@mailexternos.com.ar>
2015-09-20 13:38:12 -0300

    Bug Fixing for use MySql via EF6 Add SetupFixture

---
+++ DAO.EntityFramework.Testing/SetupFixture.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using MySql.Data.Initializer;
+using NUnit.Framework;
+
+namespace DAO.EntityFramework.Testing
+{
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            DbConfiguration.SetConfiguration(new MySqlConfiguration());
+        }
+    }
+}
